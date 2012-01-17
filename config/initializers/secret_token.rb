6732a731fe0618f97e9ebed8a3ce749d27845765
Daniel Carneiro <daniel.carneiro@biodevice.pt>
2012-01-17 18:05:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VitalWatcher::Application.config.secret_token = '12bb1aa418d02bc5a9a02ae5e04c4871e7cc8827c263246e273b309fbe70e149feabe9651ccd6590a9388a553e5d2bcf3bb0ca032d65f688d5323d02acb06836'
